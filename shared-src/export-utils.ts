fa274fd42f7b6d0371930410326c16068069720a
origami-z <davidcui2@me.com>
2023-01-09 16:53:00 +0000

    Adjust JSON export format

--- shared-src/export-utils.ts
+++ shared-src/export-utils.ts
@@ -5,9 +5,22 @@ export const convertToCsvDataUri = <T extends unknown[]>(data: T): string => {
   return "data:text/csv;charset=utf-8," + encodeURIComponent(rowsString);
 };
 
-export const convertToJsonDataUri = <T extends unknown[]>(data: T): string => {
+export const convertToJsonDataUri = <T extends { [key: string]: string }[]>(
+  data: T
+): string => {
+  const contentConverted: { [key: string]: string } = {};
+  for (const row of data) {
+    if (row.key && row.characters) {
+      contentConverted[row.key] = row.characters;
+    }
+  }
+  const exportJson = {
+    // More to come, e.g. region, language
+    dateExported: new Date().toISOString(),
+    content: contentConverted,
+  };
   return (
     "data:application/json;charset=utf-8," +
-    encodeURIComponent(JSON.stringify(data))
+    encodeURIComponent(JSON.stringify(exportJson))
   );
 };
